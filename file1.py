78295a0f984f80237bf1414de6249e81bd3e25d5
b-zebrowski <6879017+b-zebrowski@users.noreply.github.com>
2025-01-27 09:31:07 -0500

    Update file1.py
    setting it to real code.

--- file1.py
+++ file1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import requests
 # This is a single line comment, you should not see the link to https://github.com/accountnotexist/some-repo
 
 '''
@@ -7,6 +8,5 @@ Now lets check a multi-line comment, you should not see a link to https://github
 """
 Another multi-line comment, you should not see a link to https://github.com/accountnotexist/some-repo
 """
-this line you should see pointing towards https://github.com/accountnotexist/some-repo
-
-this line should not show up as it ties back to a specific tag https://github.com/accountnotexist/some-repo@123456
+bla = requests.get('https://github.com/accountdoesnotexist/some-repo') # this line you should see pointing to a non-existent user
+bla2 = requests.get('https://github.com/accountdoesnotexist/some-repo@123456') # same user, but calling a specific tag
